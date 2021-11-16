f8958a4d2a7c3639df496d42b6ff4199edfdad7e
Jack-Patterson <jackpatterson.10.02@gmail.com>
2021-11-16 09:21:43 +0000

    16/11/21 OOP Mini Project Daughts Jack Patterson

--- Draughts/TestDriver.java
+++ Draughts/TestDriver.java
@@ -4,6 +4,6 @@ public class TestDriver {
     public static void main(String[] args) {
         Piece test = new Piece(4, 4, "black", true, true);
 
-        System.out.println(test.toString());
+        System.out.println(test);
     }
 }
